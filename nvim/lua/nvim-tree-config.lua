988eb3c0a71862ef750a8800265b213a3a9f0ea8
Nydragon <6582485n@proton.me>
2023-02-12 22:30:18 +0100

    focus opened file in nvim

--- nvim/lua/nvim-tree-config.lua
+++ nvim/lua/nvim-tree-config.lua
@@ -2,12 +2,21 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup()
-require'nvim-tree'.toggle(false,true)
 
---local function open_nvim_tree()
+local function open_nvim_tree(data)
 
-  ---- open the tree
-  --require("nvim-tree.api").tree.open()
---end
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
 
---vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  if not real_file and not no_name then
+    return
+  end
+
+  -- open the tree, find the file but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
