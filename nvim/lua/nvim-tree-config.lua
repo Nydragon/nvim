924df96e8d9a2664b35710d0c0bfd368abc1fb43
Nydragon <6582485n@proton.me>
2023-02-12 20:34:04 +0100

    simplified tree auto opening mechanism

--- nvim/lua/nvim-tree-config.lua
+++ nvim/lua/nvim-tree-config.lua
@@ -2,11 +2,12 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup()
+require'nvim-tree'.toggle(false,true)
 
-local function open_nvim_tree()
+--local function open_nvim_tree()
 
-  -- open the tree
-  require("nvim-tree.api").tree.open()
-end
+  ---- open the tree
+  --require("nvim-tree.api").tree.open()
+--end
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+--vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
