a4af16f614d00d51e58292776900b02a9b499ec0
Nydragon <6582485n@proton.me>
2023-12-20 12:20:31 +0900

    add sway config

--- nvim/lua/telescope-config.lua
+++ nvim/lua/telescope-config.lua
@@ -1,8 +1,15 @@
-require('telescope').setup()
+require("telescope").setup({
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--no-ignore", "--glob", "!**/.git/*" },
+		},
+	},
+})
 
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
