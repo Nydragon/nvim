ffb7079a15fbbfd5d6db09bd03ba0fddfa8a9772
Nydragon <contact@ccnlc.eu>
2024-05-28 14:48:01 +0900

    fix: close nvim tree if it is the last buffer

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -7,15 +7,13 @@ local function open_nvim_tree(data)
 		return
 	end
 
-	vim.notify(vim.loop.cwd())
-
 	-- buffer is a real file on the disk
 	local real_file = vim.fn.filereadable(data.file) == 1
 
 	-- buffer is a [No Name]
-	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+	--local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-	if not real_file and not no_name then
+	if not real_file then --and not no_name then
 		return
 	end
 
@@ -27,11 +25,31 @@ local function open_nvim_tree(data)
 	end
 
 	-- open the tree, find the file but don't focus it
-	api.tree.open({
+	api.tree.toggle({
 		focus = false,
 		find_file = true,
 		path = vim.loop.cwd(),
 	})
 end
 
-vim.api.nvim_create_autocmd({ "BufAdd" }, { callback = open_nvim_tree })
+vim.api.nvim_create_autocmd({ "BufEnter" }, { callback = open_nvim_tree })
+
+-- From https://github.com/nvim-tree/nvim-tree.lua/issues/1368#issuecomment-1512248492
+vim.api.nvim_create_autocmd("QuitPre", {
+	callback = function()
+		local invalid_win = {}
+		local wins = vim.api.nvim_list_wins()
+		for _, w in ipairs(wins) do
+			local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+			if bufname:match("NvimTree_") ~= nil then
+				table.insert(invalid_win, w)
+			end
+		end
+		if #invalid_win == #wins - 1 then
+			-- Should quit, so we close all invalid windows.
+			for _, w in ipairs(invalid_win) do
+				vim.api.nvim_win_close(w, true)
+			end
+		end
+	end,
+})
