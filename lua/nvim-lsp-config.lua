2d5c0f0c3f970901db7bb67190f5fe1a59c7185f
Nydragon <contact@ccnlc.eu>
2024-08-25 18:43:35 +0200

    feat: add qmlls

--- lua/nvim-lsp-config.lua
+++ lua/nvim-lsp-config.lua
@@ -54,6 +54,8 @@ lspconfig.nixd.setup({})
 
 lspconfig.r_language_server.setup({})
 
+lspconfig.qmlls.setup({})
+
 lspconfig.lua_ls.setup({
 	on_init = function(client)
 		local path = client.workspace_folders[1].name
